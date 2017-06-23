4ff398849545afd3ea5581368be500eff806c8ee
guillaume volgyesi <guillaume volgyesi>
2017-06-23 20:13:18 +0200

    Work in progress for attacking ennemies

---
+++ Assets/Script/Manager/Ennemies/GroundedEnnemies.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundedEnnemies : MonoBehaviour
+{
+
+	
+	void Start ()
+    {
+		
+	}
+
+   
+	void Update ()
+    {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    { 
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Script/Manager/Player/PlayerManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    Rigidbody2D body;
+    float horizontal;
+    float vertical;
+    Vector2 horizontalMovement;
+    [SerializeField] float speed;
+    [SerializeField] bool IsTurnedRight = true;
+
+    [SerializeField]
+    BoxCollider2D[]hitZones = new BoxCollider2D[4];
+    float hitTimer;
+    bool hit = false;
+
+    void Start ()
+    {
+        body = GetComponent<Rigidbody2D>();       
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        horizontalMovement = new Vector2(
+                    speed * horizontal,0);
+        
+
+        if (horizontal > 0 && !IsTurnedRight)
+        {
+                IsTurnedRight = true;
+        }
+        if(horizontal < 0 && IsTurnedRight)
+        {        
+                IsTurnedRight = false;                
+        }
+        if(Input.GetKey(KeyCode.Mouse0))
+        {
+            AttackDirection();               
+        }       
+      /*  for(int i = 0; i < hitZones.Length; i ++)
+        {
+            hitZones[i].enabled = false;
+        }*/
+    }
+    void FixedUpdate()
+    {
+        body.velocity = horizontalMovement;
+    }
+    void AttackDirection()
+    {
+        if(horizontal > 0)
+        {
+            hitZones[0].enabled = true;
+        }
+        if (horizontal < 0)
+        {
+            hitZones[1].enabled = true;
+        }
+        if(vertical > 0)
+        {
+            hitZones[2].enabled = true;
+        }
+        if (vertical < 0)
+        {
+            hitZones[3].enabled = true;
+        }
+    }
+}
+
